f0782088a6c05ba5279e243376d19b96f6a00d89
caozhongpa <yzfff6@outlook.com>
2023-07-21 15:59:17 +0800

    feat: complete gene regulation part

--- spaTrack/single_time/gene_regulation.py
+++ spaTrack/single_time/gene_regulation.py
@@ -39,6 +39,7 @@ class Model(nn.Module):
 
         self.linear=nn.Linear(n_gene,n_TF)
 
+
     def forward(self,x: torch.Tensor) -> torch.Tensor:
         """
         Give the gene changes of cells, and get the relationship between genes and TFs through linear regression.
@@ -64,63 +65,79 @@ class Trainer():
 
     parameters
     ----------
+    type
+        The Data type. Including dual time point data of two slices and pseudotime data of one slice.
     expression_matrix_path
         The path of the expression matrix file.
-    ptime_path
-        The path of the ptime file, used to determine the sequence of the ptime data.
     tfs_path
         The path of the tf names file.
     cell_mapping_path
         The path of the cell mapping file, where column `index_x` indicates the start cell and column `index_y` indicates the end cell.
+    ptime_path
+        The path of the ptime file, used to determine the sequence of the ptime data.
     min_cells, optional
         The minimum number of cells for gene filtration, by default 100.
-    cell_num_each_time, optional
+    cell_divide_per_time, optional
         The cell number generated at each time point using the meta-analysis method, by default 500.
-    random_select_cell_num, optional
-        The number of random cells to use when generating cells, by default 10
+    cell_select_per_time, optional
+        The number of randomly selected cells at each time point.
+    cell_generate_per_time, optional
+        The number of cells generated at each time point.
+    train_ratio
+        Ratio of training data.
     use_gpu, optional
         Whether to use gpu, by default True.
     """
     def __init__(
         self,
-        type: Literal['2_time','p_time'],
+        data_type: Literal['2_time','p_time'],
         expression_matrix_path: Union[str, List[str]],
         tfs_path: str,
         cell_mapping_path: str = None,
         ptime_path: str = None,
         min_cells: Union[int,List[int]] = 100,
-        cell_num_each_time: int = 500,
-        random_select_cell_num: int = 10,
-        sample_num: int =10000,
-        cell_num: int =10,
+        cell_divide_per_time: int = 80,
+        cell_select_per_time: int = 10,
+        cell_generate_per_time: int = 500,
+        train_ratio: float = 0.8,
         use_gpu: bool = True,
     ) -> None:
+        self.train_ratio=train_ratio
         # read gene expression, cell ptime and tfs files. 
-        if type=='p_time':
+        if data_type=='p_time':
+            if type(expression_matrix_path) == list:
+                expression_matrix_path=expression_matrix_path[0]
             self.adata=sc.read(expression_matrix_path)
             self.adata.obs['ptime']=pd.read_table(ptime_path,index_col=0)
             self.tfs_path = tfs_path
 
             # filter out genes expressed in few cells
             sc.pp.filter_genes(self.adata, min_cells=min_cells,inplace=True)
+            print(f'Genes expressed in less than {min_cells} cells have been filtered.')
 
-            all_tfs=pd.read_csv(self.tfs_path,index_col=0)
+            all_tfs=pd.read_csv(self.tfs_path)
             self.genes=self.adata.var_names
-            self.tfs=self.genes.str.lower().intersection(all_tfs.gene.str.lower().tolist())
+            self.tfs=self.genes.intersection(all_tfs.gene.tolist())
 
-            self.generate_meta_data_p_time(cell_num_each_time,random_select_cell_num)
+            self.generate_data_p_time(cell_divide_per_time, cell_select_per_time, cell_generate_per_time)
         elif type=='2_time':
             # 2_time Pattern need two adata files, the cell mapping file and the tfs file.
+            if type(expression_matrix_path)!=list or len(expression_matrix_path)!=2:
+                sys.exit('In 2_time mode, a list of gene expression matrix paths for two slices must be provided.')
+
             self.adata1=sc.read(expression_matrix_path[0])
             self.adata2=sc.read(expression_matrix_path[1])
             self.cell_mapping=pd.read_csv(cell_mapping_path,index_col=0)
             self.tfs_path = tfs_path
 
             # filter genes
+            if type(min_cells)!=list or len(min_cells)!=2:
+                sys.exit('In 2_time mode, you should provide a list contains two min_cells parameters to filter two adata files.')
+
             sc.pp.filter_genes(self.adata1, min_cells=min_cells[0],inplace=True)
             sc.pp.filter_genes(self.adata2, min_cells=min_cells[1],inplace=True)
 
-            self.generate_meta_data_2_time(sample_num,cell_num)
+            self.generate_data_2_time(cell_generate_per_time,cell_select_per_time)
 
         gpu=torch.cuda.is_available() and use_gpu
         if gpu:
@@ -129,13 +146,23 @@ class Trainer():
             self.device = torch.device('cpu')
 
 
-    def getMetaData(self,sample_num,cell_num):
+    def getMetaData(self,cell_generate_per_time,cell_select_per_time) -> None:
+        """
+        Randomly sample cell expression and generate meta data.
+
+        Parameters
+        ----------
+        cell_generate_per_time
+            The amount of data generated at each time point.
+        cell_select_per_time
+            The amount of data randomly selected at each time point.
+        """
         new_data_in=[]
         new_data_out=[]
-        for i in range(sample_num):
-            cell_indexes=random.sample(range(self.input_data.shape[0]),cell_num)
-            new_data_in.append(np.sum(self.input_data[cell_indexes],axis=0))
-            new_data_out.append(np.sum(self.output_data[cell_indexes],axis=0))
+        for i in range(cell_generate_per_time):
+            cell_indexes=random.sample(range(self.input_data.shape[0]),cell_select_per_time)
+            new_data_in.append(np.mean(self.input_data[cell_indexes],axis=0))
+            new_data_out.append(np.mean(self.output_data[cell_indexes],axis=0))
         
         self.input_data=np.stack(new_data_in)
         self.output_data=np.stack(new_data_out)
@@ -146,8 +173,26 @@ class Trainer():
         training_times: int = 10, 
         iter_times: int = 30, 
         mapping_num: int = 3000,
-        filename='afterScale_100.csv',
+        filename: str = 'weights.csv',
     ) -> None:
+        """
+        Run the trainer.
+
+        Parameters
+        ----------
+        training_times
+            Number of times to randomly initialize the model and retrain.
+            (Default: 10)
+        iter_times
+            The number of iterations for each training model, by default 30.
+            (Default: 30)
+        mapping_num
+            The number of mapping weight pairs.
+            (Default: 3000)
+        filename
+            The saved file name.
+            (Default: 'weights.csv')
+        """
         self.mapping_num=mapping_num
         self.all_gtf = []
         
@@ -203,7 +248,28 @@ class Trainer():
         print(f'Weight relationships of tfs and genes are stored in {filename}.')
 
 
-    def plot_scatter(self,type='raise',num_rows=2,num_cols=5,fig_width=20,fig_height=8,):
+    def plot_scatter(self,type: str = 'raise',num_rows: int=2,num_cols:int=5,fig_width:int=20,fig_height:int=8,)-> None:
+        """
+        Show the relationship between TF and gene changes through scatter plot.
+
+        Parameters
+        ----------
+        type
+            The displayed relationship types, include raise and drop.
+            (Default: 'raise')
+        num_rows
+            The number of rows in the graph.
+            (Default: 2)
+        num_cols
+            The number of columns in the graph.
+            (Default: 5)
+        fig_width
+            The width of the image.
+            (Default: 20)
+        fig_height
+            The height of the image.
+            (Default: 8)
+        """        
         if self.mapping_num<(num_rows*num_cols):
             sys.exit('Mapping_num is less than the product of num_rows and num_cols, please run again and increase the mapping_num.')
         fig, axs = plt.subplots(num_rows, num_cols,figsize=(fig_width,fig_height),)
@@ -220,13 +286,22 @@ class Trainer():
             ax.scatter(x, y, s=1, color='#377eb8')
             ax.set_title(f"{self.tfs[TF.item()]}, {self.genes[gene.item()]}")
             # ax.set_aspect('equal')
-        # 展示图形
         # plt.tight_layout()
         # fig.subplots_adjust(left=0.06)
         plt.show()
 
 
-    def generate_meta_data_2_time(self,sample_num,cell_num):
+    def generate_data_2_time(self,cell_generate_per_time,cell_select_per_time):
+        """
+        Generate data in the 2_time mode.
+
+        Parameters
+        ----------
+        cell_generate_per_time
+            The amount of data generated at each time point.
+        cell_select_per_time
+            The amount of data randomly selected at each time point.
+        """        
         # only use the mapping cells
         self.adata1=self.adata1[self.cell_mapping.index_x]
         self.adata2=self.adata2[self.cell_mapping.index_y]
@@ -250,57 +325,58 @@ class Trainer():
         self.input_data = np.array(delta_gene,dtype=np.float32)
         self.output_data = np.array(self.adata2.X @ self.T.T,dtype=np.float32)
 
-        self.getMetaData(sample_num,cell_num)
+        self.getMetaData(cell_generate_per_time,cell_select_per_time)
 
         # normalize data 
-        nor_input_data=(self.input_data-self.input_data.mean(axis=0))/(self.input_data.max(axis=0)-self.input_data.min(axis=0))
-        nor_output_data=(self.output_data-self.output_data.min(axis=0))/(self.output_data.max(axis=0)-self.output_data.min(axis=0))
+        self.input_data=(self.input_data-self.input_data.mean(axis=0))/(self.input_data.max(axis=0)-self.input_data.min(axis=0))
+        self.output_data=(self.output_data-self.output_data.min(axis=0))/(self.output_data.max(axis=0)-self.output_data.min(axis=0))
 
         # # shuffle the cell order (2_time data has been shuffled in the meta step)
         # permuted_idxs=np.random.permutation(self.input_data.shape[0])
         # self.input_data=nor_input_data[permuted_idxs]
         # self.output_data=nor_output_data[permuted_idxs]
 
-        train_ratio=0.8
-        num_samples=self.input_data.shape[0]
-        train_size=int(num_samples*train_ratio)
-
-        train_data_in=torch.from_numpy(nor_input_data[:train_size,:])
-        train_data_out=torch.from_numpy(nor_output_data[:train_size,:])
-        test_data_in=torch.from_numpy(nor_input_data[train_size:,:])
-        test_data_out=torch.from_numpy(nor_output_data[train_size:,:]) 
-
-        train_set=TensorDataset(train_data_in,train_data_out)
-        test_set=TensorDataset(test_data_in,test_data_out)
-        batch_size=32
-        self.train_dl=DataLoader(train_set,batch_size=batch_size,shuffle=True)
-        self.test_dl=DataLoader(test_set,batch_size=batch_size)
+        self.get_dataloader()
 
 
-    def generate_meta_data_p_time(self,cell_num_each_time,random_select_cell_num) -> None:
-        sub_index=self.sort_idx()
+    def generate_data_p_time(self,cell_divide_per_time, cell_select_per_time, cell_generate_per_time) -> None:
+        """
+        Generate data in the 2_time mode.
 
-        mean_data=[]
-        origin_data=[]  # time_point * cell * gene expression
+        Parameters
+        ----------
+        cell_divide_per_time
+            The number of divided cells per time point.
+        cell_select_per_time
+            The amount of data randomly selected at each time point.
+        cell_generate_per_time
+            The amount of data generated at each time point.
+        """        
+        sub_index=self.sort_idx(cell_divide_per_time)
+
+        self.mean_data=[]   # mean expression of genes at each time point
+        self.origin_data=[]  # time_point * cell * gene expression
         for i in range(len(sub_index)):
-            origin_data.append(np.array(self.adata[sub_index[i]].X))
-            mean_data.append(self.adata[sub_index[i]].X.mean(axis=0))
-        mean_data=np.array(mean_data)
-        origin_data=np.stack(origin_data)
+            self.origin_data.append(np.array(self.adata[sub_index[i]].X))
+            self.mean_data.append(self.adata[sub_index[i]].X.mean(axis=0))
+        self.mean_data=np.array(self.mean_data)
+        self.origin_data=np.stack(self.origin_data)
 
         self.get_one_hot()
 
         input_data=[]
         output_data=[]
-        for i in range(1,len(origin_data)):
-            for j in range(cell_num_each_time):
-                random_idxs=random.sample(range(len(origin_data[i])),random_select_cell_num)
-                meta_expr=np.array(origin_data[i][random_idxs].mean(axis=0))
-                delta_gene=meta_expr-mean_data[i-1]
-                tf_expr=(origin_data[i][random_idxs].mean(axis=0))@self.T.T
-                
+        for i in range(1,len(self.origin_data)):
+            for j in range(cell_generate_per_time):
+                random_idxs=random.sample(range(len(self.origin_data[i])),cell_select_per_time)
+                meta_expr=np.array(self.origin_data[i][random_idxs].mean(axis=0))
+                delta_gene=meta_expr-self.mean_data[i-1]
+                tf_expr=meta_expr@self.T.T
+
                 input_data.append(delta_gene)
                 output_data.append(tf_expr)
+        print(f"{(len(self.origin_data)-1)} groups of new data were generated, each with {cell_generate_per_time} meta cells.")
+
         input_data=np.array(input_data,dtype=np.float32)
         output_data=np.array(output_data,dtype=np.float32)
 
@@ -311,7 +387,14 @@ class Trainer():
         self.input_data=nor_input_data[permuted_idxs]
         self.output_data=nor_output_data[permuted_idxs]
 
-        train_ratio=0.8
+        self.get_dataloader()
+
+
+    def get_dataloader(self):
+        """
+        Convert data to dataloader form.
+        """        
+        train_ratio=self.train_ratio
         num_samples=self.input_data.shape[0]
         train_size=int(num_samples*train_ratio)
 
@@ -328,6 +411,19 @@ class Trainer():
 
 
     def plot_gene_regulation(self,min_weight,min_node_num,cmap='coolwarm'):
+        """
+        Draw the gene regulation network graph.
+
+        Parameters
+        ----------
+        min_weight
+            Filter relation pairs whose weight is less than min_weight.
+        min_node_num
+            Min node numbef of Tfs.
+        cmap
+            Platte used..
+            (Default: 'coolwarm')
+        """        
         df=self.network_df
         df=df.loc[df['weight'].abs()>min_weight]
         print(f'num of weight pairs after weight filtering: {len(df)}')
@@ -368,28 +464,36 @@ class Trainer():
 
         Returns
         -------
-            one-hot matrix from TFs to genes
+            One-hot matrix from TFs to genes.
         """
-        vectorizer=CountVectorizer(vocabulary=self.genes.str.lower().tolist())   # lowercase gene names
+        vectorizer=CountVectorizer(vocabulary=self.genes.tolist(),lowercase=False)   # lowercase gene names
         self.T = vectorizer.fit_transform(self.tfs).toarray()
 
 
-    def sort_idx(self):
+    def sort_idx(self, cell_divide_per_time) -> np.array:
         """
-        _summary_
+        Divide the cells in the pseudo-time series to obtain cell sets of different time segments.
+
+        Parameters
+        ----------
+        cell_divide_per_time
+            The number of divided cells per time point.
 
         Returns
-        -------
-            _description_
-        """
+        -----------
+        np.array
+            An array to divide cells.
+        """        
         ptime=self.adata.obs['ptime']
 
-        ptime_0_index=ptime[ptime==0].index
-        ptime_1_index=ptime[ptime==1].index
+        ptime_0_idx=ptime[ptime==0].index
+        ptime_1_idx=ptime[ptime==1].index
 
-        middle_idx=list(self.adata.obs.index[len(ptime_0_index):len(self.adata)-len(ptime_1_index)])
-        sub_length=80
+        middle_idx=list(self.adata.obs.index[len(ptime_0_idx):len(self.adata)-len(ptime_1_idx)])
+        sub_length=cell_divide_per_time
         sub_index=[middle_idx[i:i+sub_length] for i in range(0, len(middle_idx), sub_length)][:-1]
+
+        print(f'{len(self.adata)} cells were divided into {len(sub_index)} groups according to the pseudo-time, with {cell_divide_per_time} cells in each group. The first and last cells are discarded.')
 
         # sub_index.insert(0,list(ptime_0_index)) # insert head indexes
         # sub_index.append(list(ptime_1_index))   # append tail inedexes
@@ -398,6 +502,25 @@ class Trainer():
 
 
     def train(self,dataloader, model, loss_fn, optimizer):
+        """
+        Train step.
+
+        Parameters
+        ----------
+        dataloader
+            Dataloader that contains the input and output data.
+        model
+            Model used to infer the realationship.
+        loss_fn
+            Loss function.
+        optimizer
+            The optimizer used to reduce the loss value.
+
+        Returns
+        -----------
+        float
+            Training loss.
+        """        
         size=len(dataloader.dataset)
         model.train()
         for batch, (X,y) in enumerate(dataloader):
@@ -418,6 +541,23 @@ class Trainer():
 
 
     def test(self,dataloader, model, loss_fn):
+        """
+        Test step
+
+        Parameters
+        ----------
+        dataloader
+            Dataloader that contains the input and output data.
+        model
+            Model used to infer the realationship.
+        loss_fn
+            The optimizer used to reduce the loss value.
+
+        Returns
+        -----------
+        float
+            Testing loss.
+        """        
         # size=len(dataloader.dataset)
         num_batches=len(dataloader)
         model.eval()
